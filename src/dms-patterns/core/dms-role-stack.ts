9ab1e0ae51a2b042347287035b42d68996b41e18
Matteo Giani <matteo.giani.87@gmail.com>
2024-01-19 16:10:04 +0000

    Added stuff for the roles.

--- src/dms-patterns/core/dms-role-stack.ts
+++ src/dms-patterns/core/dms-role-stack.ts
@@ -3,17 +3,16 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 
 import { Construct } from 'constructs';
 
+// This stack creates the DMS VPC role and the DMS Cloudwatch role.
 export class DmsVpcRoleStack extends cdk.Stack {
 
-  dsmAssumeRolePolicy: iam.Policy;
   dmsVpcRole: iam.Role;
+  dmsCloudwatchRole: iam.Role;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
-    //   const dmsAssumeRolePolicy = new iam.Policy(this, 'DmsVpcRolePolicy', {
-    this.dsmAssumeRolePolicy = new iam.Policy(this, 'DmsVpcRolePolicy', {
+    const dsmAssumeRolePolicy = new iam.Policy(this, 'DmsVpcRolePolicy', {
       statements: [
         new iam.PolicyStatement({
           effect: iam.Effect.ALLOW,
@@ -23,15 +22,22 @@ export class DmsVpcRoleStack extends cdk.Stack {
       ],
     });
 
-    // Create the IAM role for DMS
     this.dmsVpcRole = new iam.Role(this, 'DmsVpcRole', {
       roleName: 'dms-vpc-role',
       description: 'Service role for AWS DMS VPC management',
       assumedBy: new iam.ServicePrincipal('dms.amazonaws.com'),
     });
-
-    // Attach the existing policy to the new role
     this.dmsVpcRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonDMSVPCManagementRole'));
-    this.dmsVpcRole.attachInlinePolicy(this.dsmAssumeRolePolicy);
+    this.dmsVpcRole.attachInlinePolicy(dsmAssumeRolePolicy);
+
+
+    this.dmsCloudwatchRole = new iam.Role(this, 'DmsCloudwatchRole', {
+      roleName: 'dms-cloudwatch-logs-role',
+      description: 'Service role for AWS DMS Logs',
+      assumedBy: new iam.ServicePrincipal('dms.amazonaws.com'),
+    });
+    this.dmsCloudwatchRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonDMSCloudWatchLogsRole '));
+    this.dmsCloudwatchRole.attachInlinePolicy(dsmAssumeRolePolicy);
+
   }
 }
