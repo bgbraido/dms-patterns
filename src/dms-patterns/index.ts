5937623f4f51190d115beac7e298d2d241a805b2
Bruno Braido <Bruno.Braido@dsm.com>
2024-04-18 15:55:08 +0200

    feat: Included mysql2mysql on index.ts

--- src/dms-patterns/index.ts
+++ src/dms-patterns/index.ts
@@ -7,4 +7,5 @@ export * from './endpoints';
 export * from './mysql2s3';
 export * from './postgres2s3';
 export * from './s32s3'; // not working with serverless; issues with dms-vpc-role
+export * from './mysql2mysql';
 
